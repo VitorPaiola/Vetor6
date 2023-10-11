a7665dc3f7a4e53cdcc4e0f6e2108c2e3752cc15
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-11 16:25:14 -0300

    Adicionar arquivos de projeto.

---
+++ Vetor6/Program.cs
@@ -0,0 +1,22 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+string[] nomes = new string[N];
+int[] idades = new int[N];
+
+for (int i = 0; i < N; i++) {
+    string[] vet = Console.ReadLine().Split(' ');
+    nomes[i] = vet[0];
+    idades[i] = int.Parse(vet[1]);
+}
+
+int maiorIdade = idades[0];
+int posicaoMaiorIdade = 0;
+
+for (int i = 0; i < N; i++) {
+    if (idades[i] > maiorIdade) {
+        maiorIdade = idades[i];
+        posicaoMaiorIdade = i;
+    }
+}
+
+Console.WriteLine("Pessoa mais velha: " + nomes[posicaoMaiorIdade]);
